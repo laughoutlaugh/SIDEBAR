c8fd0c781efd9951df670adb803936836b2ef99f
niko <64sparkles328@gmail.com>
2026-08-21 17:05:51 -0400

    list_screen implementation

--- main/main.py
+++ main/main.py
@@ -1,55 +1,15 @@
-'''
 import pygame
+from pygame.sprite import collide_rect_ratio
 
 from ui.screen import Screen
 from ui.main_menu import MainMenu
-
-
-def main():
-    pygame.init()
-
-    screen = Screen()
-    menu = MainMenu(screen)
-    running = True
-
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-
-            elif event.type == pygame.KEYDOWN: # On buttonpress
-                if event.key == pygame.K_UP: # Up arrow
-                    menu.move_up()
-                elif event.key == pygame.K_DOWN: # Down arrow
-                    menu.move_down()
-                elif event.key == pygame.K_RIGHT: # Right arrow
-                    selected = menu.select()
-                    print("Selected:", selected)
-
-        menu.draw()
-        pygame.display.flip()
-        screen.clock.tick(60)
-
-    pygame.quit()
-
-
-if __name__ == "__main__":
-    main()
-
-'''
-
-# Testing list_screen class
-
-import pygame
-
-from ui.screen import Screen
 from ui.list_screen import ListScreen
 
 
 def main():
     pygame.init()
-
     screen = Screen()
+    main_menu = MainMenu(screen)
 
     songs = [
         "Song One",
@@ -66,12 +26,32 @@ def main():
         "Song Twelve",
     ]
 
-    list_screen = ListScreen(
-        screen,
-        "Songs",
-        songs
-    )
+    albums = [
+        "Album One",
+        "Album Two",
+        "Album Three",
+        "Album Four",
+        "Album Five",
+        "Album Six",
+        "Album Seven",
+    ]
 
+    artists = [
+        "Artist One",
+        "Artist Two",
+        "Artist Three",
+        "Artist Four",
+        "Artist Five",
+        "Artist Six",
+        "Artist Seven",
+        "Artist Eight",
+    ]
+
+    song_list = ListScreen(screen, "Songs", songs)
+    album_list = ListScreen(screen, "Albums", albums)
+    artist_list = ListScreen(screen, "Artists", artists)
+
+    current_screen = "main_menu"
     running = True
 
     while running:
@@ -83,20 +63,96 @@ def main():
 
             elif event.type == pygame.KEYDOWN:
 
-                if event.key == pygame.K_UP:
-                    list_screen.move_up()
+                # --------------------------
+                # MAIN MENU
+                # --------------------------
 
-                elif event.key == pygame.K_DOWN:
-                    list_screen.move_down()
+                if current_screen == "main_menu":
 
-                elif event.key == pygame.K_RIGHT:
-                    selected = list_screen.select()
-                    print("Selected:", selected)
+                    if event.key == pygame.K_UP:
+                        main_menu.move_up()
 
-        list_screen.draw()
+                    elif event.key == pygame.K_DOWN:
+                        main_menu.move_down()
+
+                    elif event.key == pygame.K_RIGHT:
+                        selected = main_menu.select()
+
+                        if selected == "Songs":
+                            current_screen = "songs"
+                        elif selected == "Albums":
+                            current_screen = "albums"
+                        elif selected == "Artists":
+                            current_screen = "artists"
+                        elif selected == "Settings":
+                            print("Settings not implemented yet </3")
+
+                # --------------------------
+                # SONG LIST
+                # --------------------------
+
+                elif current_screen == "songs":
+
+                    if event.key == pygame.K_UP:
+                        song_list.move_up()
+                    elif event.key == pygame.K_DOWN:
+                        song_list.move_down()
+                    elif event.key == pygame.K_RIGHT:
+                        selected = song_list.select()
+                        print("Selected song:", selected)
+
+                # --------------------------
+                # ALBUM LIST
+                # --------------------------
+
+                elif current_screen == "albums":
+
+                    if event.key == pygame.K_UP:
+                        album_list.move_up()
+                    elif event.key == pygame.K_DOWN:
+                        album_list.move_down()
+                    elif event.key == pygame.K_RIGHT:
+                        selected = album_list.select()
+                        print("Selected album:", selected)
+
+                # --------------------------
+                # ARTIST LIST
+                # --------------------------
+
+                elif current_screen == "artists":
+
+                    if event.key == pygame.K_UP:
+                        artist_list.move_up()
+                    elif event.key == pygame.K_DOWN:
+                        artist_list.move_down()
+                    elif event.key == pygame.K_RIGHT:
+                        selected = artist_list.select()
+                        print("Selected artist:", selected)
+
+                # --------------------------
+                # BACK
+                # --------------------------
+
+                if event.key == pygame.K_LEFT:
+                    current_screen = "main_menu"
+
+        # --------------------------
+        # DRAW CURRENT SCREEN
+        # --------------------------
+
+        if current_screen == "main_menu":
+            main_menu.draw()
+
+        elif current_screen == "songs":
+            song_list.draw()
+
+        elif current_screen == "albums":
+            album_list.draw()
+
+        elif current_screen == "artists":
+            artist_list.draw()
 
         pygame.display.flip()
-
         screen.clock.tick(60)
 
     pygame.quit()
